30b7793685df64692bc6b917424a8e9574b8135f
somoire <rayr30667@gmail.com>
2019-05-17 05:34:07 -0400

    Email sending functionality added to the code

--- clone/email.py
+++ clone/email.py
@@ -1,2 +1,16 @@
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+
+def send_welcome_email(name,receiver):
+    #Creating a message subject and sender
+    subject = 'Welcome to this amazing instagram application dummy'
+    sender = 'rayr30667@gmail.com'
+
+    #Passing in the context variables
+    text_content = render_to_string('email/igemail.txt',{"name":name})
+    html_content = render_to_string('email/igemail.html',{"name":name})
+
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
+    
