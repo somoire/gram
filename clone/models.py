ca24b9bd95c463b16f24824264b53c103712eddc
somoire <rayr30667@gmail.com>
2019-05-18 14:58:52 -0400

    requirements.txt file added

--- clone/models.py
+++ clone/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.db.models import Q
 
+import datetime as dt 
+
+Gender = (
+    ('Male', 'Male'),
+    ('Female','Female'),
+)
 # Create your models here.
+class Location(models.Model):
+    location = models.CharField
