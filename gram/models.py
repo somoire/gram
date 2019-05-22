8f7a9284a87ab822bc1aded42253c6feb5e5d134
somoire <rayr30667@gmail.com>
2019-05-22 05:54:44 -0400

    additional characters

--- gram/models.py
+++ gram/models.py
@@ -11,7 +11,7 @@ fs = FileSystemStorage(location='./media/')
 # Create your models here.
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete = models.CASCADE)
-  bio = models.TextField(max_length = 30, blank = True)
+  bio = models.TextField(max_length = 150, blank = True)
   website = models.CharField(max_length=30, blank =True)
   phone_number = models.IntegerField(blank =True, null = True)
   location = models.CharField(max_length = 30, blank =True)
